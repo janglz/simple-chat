a95a2aa6867811e2a051cee859ce96b042861bd0
Janglz <wpidish@gmail.com>
2021-06-30 13:29:28 +0300

    removed trash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import ChatWindow from './components/ChatWindow'
 import SendMessageForm from './components/SendMessageForm'
 import Firebase, { FirebaseContext } from './components/Firebase';
